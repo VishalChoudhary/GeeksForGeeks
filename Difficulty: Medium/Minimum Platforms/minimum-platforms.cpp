67fcf83149db01fda2a246e7c8e44c84d4e4d3cf
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-06-28 12:31:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -1,69 +1,24 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
     int findPlatform(vector<int>& arr, vector<int>& dep) {
         // Your code here
-        int n=arr.size();
         sort(arr.begin(),arr.end());
         sort(dep.begin(),dep.end());
         int i=0,j=0;
-        int cnt=0, maxCnt=INT_MIN;
-        while(i<n){
+        int cnt=0,max_cnt=INT_MIN;
+        while(i<arr.size()){
             if(arr[i]<=dep[j]){
-                cnt++;
+                cnt+=1;
                 i++;
             }
-            else{
-                cnt--;
+            else {
+                cnt-=1;
                 j++;
             }
-            maxCnt=max(cnt,maxCnt);
+            max_cnt=max(cnt,max_cnt);
         }
-        return maxCnt;
+        return max_cnt;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr, dep;
-        string input;
-
-        // Read first array (arr)
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        // Read second array (dep)
-        getline(cin, input);
-        stringstream ss2(input);
-        while (ss2 >> number) {
-            dep.push_back(number);
-        }
-
-        // Assuming Solution class is defined elsewhere with a method findPlatform
-        Solution ob;
-        cout << ob.findPlatform(arr, dep);
-
-        cout << endl;
-        cout << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
